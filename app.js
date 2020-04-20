a6f5d0476c10508982cdf04bf9c01675c524b06e
anirudh <anirudhvs@outlook.com>
2020-04-20 22:36:07 +0530

    Updated Time

--- app.js
+++ app.js
@@ -1,25 +1,23 @@
 let k = 26;
-let corr = 1;
+let correct = 1;
 const wrap = document.querySelector('.wrapper');
 let arr = [];
 for (let i = 0; i < 25; i++) {
   arr.push(i + 1);
 }
 
-for (let i = 0; i < 25; i++) {
-  let j = (Math.random() * 100) % 24;
-  let temp = arr.splice(j, 1);
-  arr.push(temp);
-}
+//randomising values
+// for (let i = 0; i < 25; i++) {
+//   let j = (Math.random() * 100) % 24;
+//   let temp = arr.splice(j, 1);
+//   arr.push(temp);
+// }
 
 let flag = 0;
 let startTime;
-let endTime;
-
 wrap.addEventListener('click', function (e) {
 
   if (flag == 0) {
-    startTime = new Date()
     wrap.textContent = '';
     for (let i = 0; i < 25; i++) {
       const block = document.createElement('span');
@@ -27,20 +25,42 @@ wrap.addEventListener('click', function (e) {
       block.classList.add('elements');
       block.id = `${i}`;
       wrap.appendChild(block);
+      startTime = Date.now();
+      k = 26;
     }
-
   }
 
 
+  //checking Correct or Wrong Option
   if (flag) {
-    if (parseInt(e.target.textContent) == corr) {
+    if (parseInt(e.target.textContent) == correct) {
       e.target.textContent = k++;
-      corr++;
+      console.log(k);
+      if (k > 41) {
+        e.target.textContent = '';
+      }
+      correct++;
     }
-
 
   }
   flag++;
-  console.log(new Date() - startTime);
+  console.log(correct);
 
+
+  let time = document.querySelector('.timer');
+  if (correct == 40) {
+    time.textContent = '';
+  }
+  let counter;
+  if (correct <= 40) {
+    counter = setInterval(() => {
+
+      let elapsedTime = Date.now() - startTime;
+
+      time.textContent = elapsedTime;
+      if (correct > 40)
+        clearInterval(counter);
+    }, 1000);
+  }
+  console.log(counter);
 });
